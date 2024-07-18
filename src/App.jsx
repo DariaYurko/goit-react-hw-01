8169055118fb41b0b75f64938354a18f6b5aee50
DariaYurko <dariayurkodev@gmail.com>
2024-07-18 10:58:34 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+import Profile from './components/Profile'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+  
+}
+
+export default App
