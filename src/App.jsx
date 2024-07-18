113f056b1156c73a5ea7530f6905d7acf3246522
DariaYurko <dariayurkodev@gmail.com>
2024-07-18 12:20:43 +0300

    refactor: add userData.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+// import { useState } from 'react';
+import userData from './userData.json';
 
 import './App.css';
-import Profile from './components/Profile';
+import Profile from './components/profile/Profile';
 
 function App() {
-  const userData = {
-    username: 'Jacques Gluke',
-    tag: 'jgluke',
-    location: 'Ocho Rios, Jamaica',
-    avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
-    stats: {
-      followers: 5603,
-      views: 4827,
-      likes: 1308,
-    },
-  };
-
   return (
-    <div>
+    <div className="container">
       <Profile
         username={userData.username}
         tag={userData.tag}
