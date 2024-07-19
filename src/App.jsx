059fbe74bd194fe04283b15393f8c9263e98ca74
DariaYurko <dariayurkodev@gmail.com>
2024-07-19 12:54:52 +0300

    add component TransactionHistory

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import { useState } from 'react';
 import userData from './userData.json';
 import friends from './friends.json';
+import transactionsMy from './transactions.json';
 
 import './App.css';
 import Profile from './components/Profile/Profile';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 function App() {
   return (
@@ -18,8 +20,9 @@ function App() {
         views={userData.stats.views}
         likes={userData.stats.likes}
       />
-
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactionsMy} />
     </div>
   );
 }
